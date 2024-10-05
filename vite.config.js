f4c0d07f7320093dbcdbeb7b83228bbeaa097811
shoban shaikh <shobanshaikh27@gmail.com>
2024-10-05 21:54:40 +0530

    path import addded for re-deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
-const path = require('path');
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
